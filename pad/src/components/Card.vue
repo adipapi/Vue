175692d7e0f9914cabda7b60c6d9733ead7dcd73
AdiPapi <adipapismedov@gmail.com>
2020-03-25 16:25:03 +0200

    Going home mothafucka

--- pad/src/components/Card.vue
+++ pad/src/components/Card.vue
@@ -1,7 +1,9 @@
 <template>
-  <v-card height='100%'>
-    <v-card-title class='pink white--text'> {{title}} </v-card-title>
-    <v-card-text></v-card-text>
+  <v-card height="100%">
+    <v-card-title class="pink white--text">{{title}}</v-card-title>
+    <v-card-text>
+      <v-btn> hello </v-btn>
+    </v-card-text>
   </v-card>
 </template>
 
---
+++ pad/src/components/MenuButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-btn>{{text}}</v-btn>
+</template>
+
+<script>
+
+export default {
+  name: 'MenuButton',
+    props: {
+    text: 
+    {
+        type: String,
+        required: true
+    }
+  }
+};
+</script>
